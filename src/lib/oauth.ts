246ec82c54f3eadbc96b3014852496696202a233
inam95 <mfihassan99@gmail.com>
2025-04-21 19:28:23 +0530

    fix: revert changes

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -2,15 +2,18 @@
 
 import { createAdminClient } from "@/lib/appwrite";
 import { redirect } from "next/navigation";
+import { headers } from "next/headers";
 import { OAuthProvider } from "node-appwrite";
 
 export async function signUpWithGithub() {
   const { account } = await createAdminClient();
 
+  const origin = (await headers()).get("origin");
+
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Github,
-    `${process.env.NEXT_PUBLIC_APP_URL}/oauth`,
-    `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`
+    `${origin}/oauth`,
+    `${origin}/sign-up`
   );
 
   return redirect(redirectUrl);
