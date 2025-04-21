c0725fa4cd105721843e28b81384b053867feb49
inam95 <mfihassan99@gmail.com>
2025-04-21 19:24:44 +0530

    fix: update OAuth redirect URLs to use environment variable
    - Changed redirect URLs in the OAuth route and GitHub sign-up function to utilize NEXT_PUBLIC_APP_URL for consistency and flexibility in deployment.

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -2,18 +2,15 @@
 
 import { createAdminClient } from "@/lib/appwrite";
 import { redirect } from "next/navigation";
-import { headers } from "next/headers";
 import { OAuthProvider } from "node-appwrite";
 
 export async function signUpWithGithub() {
   const { account } = await createAdminClient();
 
-  const origin = (await headers()).get("origin");
-
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Github,
-    `${origin}/oauth`,
-    `${origin}/sign-up`
+    `${process.env.NEXT_PUBLIC_APP_URL}/oauth`,
+    `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`
   );
 
   return redirect(redirectUrl);
